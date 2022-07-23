5b5a58c0878b89262b68714f9af50b54b4995bfe
disillusion021 <disillusion021@gmail.com>
2022-07-23 08:45:05 +0800

    push develop分支，测试会不会把本地的orign/develop和远程仓库的develop分支都更新到develop的版本号

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
+	// push develop分支，测试会不会把本地的orign/develop和远程仓库的develop分支都更新到develop的版本号
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
